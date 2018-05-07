aed2ed40e2223b51c8edddadea88d3d1e7b8c955
Mahiyaa <mmahia1121@gmail.com>
2018-05-07 12:33:39 -0400

    some client stuff

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {Router} from 'react-router-dom';
+import history from './history';
+import App from './app';
+
+ReactDOM.render(
+  <Provider>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
